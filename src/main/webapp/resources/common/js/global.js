1634963448b382e967cc780e67b4810d30ebfff3
BHAWANI SINGH SHEKHAWAT <bhawani.hmh@gmail.com>
2017-10-24 12:00:29 +0530

    Read JWT token and add in browser's local storage

--- src/main/webapp/resources/common/js/global.js
+++ src/main/webapp/resources/common/js/global.js
@@ -8,7 +8,8 @@ var Global = (function(){
 	
 	var ENUM_TYPE = {"POST":"POST", "PUT":"PUT","GET":"GET","DELETE":"DELETE"};
 	var ENUM_CONTENTTYPE = {"JSON":"application/json"};
-	 var TAG ="--------";
+	var accessToken;
+	var TAG ="--------";
 	/** 
 	 * Enumration for method type
 	 * @param none
@@ -30,9 +31,10 @@ var Global = (function(){
 	
 	var BASE_URL ='http://localhost:8080/';
 	var EMPLOYEE = 'Employee';
+	var LOGIN = 'Login';	
 	
 	/**
-	 * 
+	 * logging
 	 */
 	var logger = (function () {
 		var mode = "";					 
@@ -49,9 +51,68 @@ var Global = (function(){
 	})();
 	
 	var ModuleAJAXURL = {
-			ADD_EMPLOYEE:{url:"addemployee"}
+			ADD_EMPLOYEE:{url:"addemployee"},
+			LOGIN_PROCESS:{url:"loginProcess"},
+			SHOW_ADD_EMPLOYEE:{url:"showaddemployee"}
 	};
 	
+	/**
+	 * loginProcess
+	 * Call login process
+	 */
+	var loginProcess = function() {
+		var isSuccess = true;	
+		
+		var url = BASE_URL + ModuleAJAXURL.LOGIN_PROCESS.url;		
+		alert("url = " + url);
+		
+		var userName = document.getElementById('userName').value;
+		var password = document.getElementById('password').value;
+		
+		var login = {};
+		login.userName = userName;
+		login.password = password;	
+		
+		$.ajax({
+			url: url,
+			type: enumType.POST,
+			data:  JSON.stringify(login), /*use JSON.stringify() to serialize an object to JSON*/
+			contentType : enumContentType.JSON,	
+			returnType : enumContentType.JSON,
+			success: function(data, textStatus, request) {
+				try {
+					accessToken = request.getResponseHeader('Authorization');
+					
+					if (typeof(Storage) !== "undefined") {
+						localStorage.setItem("accessToken", accessToken);
+						logger.i(LOGIN, 'accessToken = ' + accessToken);
+					} else {
+						logger.i(LOGIN, 'Sorry! No Web Storage support..');
+					}				
+					
+					var url = BASE_URL + ModuleAJAXURL.SHOW_ADD_EMPLOYEE.url;		
+					logger.i(LOGIN, 'url = ' + url);
+					window.location.replace(url);					
+							
+					logger.i(LOGIN, 'After loadAddEmployee method! ');
+				} catch (exception) {
+					logger.i(LOGIN, 'Exception! ' + exception);
+				}
+			},
+			error: function() {
+				logger.i(LOGIN, 'Error! Error occured while addeing a employee');
+			},
+			complete: function() {
+				isSuccess = true;
+			}
+		});
+
+	};	
+	
+	/**
+	 * addEmployee
+	 * Call add employee
+	 */
 	var addEmployee = function() {
 		var isSuccess = true;	
 		
@@ -63,13 +124,19 @@ var Global = (function(){
 		
 		var employee = {};
 		employee.name = empName;
-		employee.salary = empSalary;	
+		employee.salary = empSalary;
+		
+		accessToken = localStorage.getItem("accessToken");		
+		logger.i(EMPLOYEE, 'accessToken = ' + accessToken);
 		
 		$.ajax({
 			url: url,
 			type: enumType.POST,
 			data:  JSON.stringify(employee), /*use JSON.stringify() to serialize an object to JSON*/
-			contentType : enumContentType.JSON,
+			contentType : enumContentType.JSON,			
+			headers: {
+				 "Authorization": "Bearer " + accessToken
+			},			
 			success: function(data) {
 				try {					
 					addEmployeeCall(data);		
@@ -89,70 +156,26 @@ var Global = (function(){
 	};
 	
 	
-	/*
+	/**
 	 * addEmployeeCall
 	 * Call to decide whether the Employee has been added sucessfully
 	 * @params _data
 	 */
 	var addEmployeeCall = function(_data) {		
 		try{		
-  		  var dataString = JSON.stringify(_data);
-  		 //alert("dataString = " + dataString);  		
+  		  var dataString = JSON.stringify(_data);  		 
   		  var dataJson = JSON.parse(dataString); 		
   		  var employee = dataJson;  		  
-  		  //console.log(dataJson);
-  		  //alert("id = " + employee.id + ", name = " + employee.name + ", salary = " + employee.salary);
-  		  
-  		  logger.i(EMPLOYEE, 'Employee added successfully name = ' + employee.name);
-  		  
+  		  logger.i(EMPLOYEE, 'Employee added successfully name = ' + employee.name);  		  
 		}catch(exception){			    		 
   		  logger.d(TAG,'Error while adding employee. error is '+exception);
   	  	}
-	};
-	//}.bind(this);
+	};	
 	
-	
-	/*$.ajax(
-	{ type: "POST",
-	 url: baseUrl + "query/",
-	contentType: "application/json; charset=utf-8",
-	 dataType: "json",
-	headers: {
-	 "Authorization": "Bearer " + accessToken	
-	},
-	data: JSON.stringify({q: text, lang: "en"}), //text is a String variable
-	success: function(data) { prepareResponse(data); },
-	error: function() { respond(messageInternalError); } });*/
-	
-	
-	
-	/*
-	type : 'POST',
-    //remove the .php from results.php.php
-    url : "http://externalsite.cpm/results.php",
-    //Add the request header
-    headers : {
-        Authorization : 'Bearer ' + 'XXXXXXXXXXXXXXXXXXXXXXXXX'
-    },
-    contentType : 'application/x-www-form-urlencoded',
-    //Add form data
-    data : {keyName : dataValue},
-    success : function(response) {
-        console.log(response);
-    },
-    error : function(xhr, status, error) {
-        var err = eval("(" + xhr.responseText + ")");
-        console.log(err);                   
-    }
-	*/
-	
-	return{
-		addEmployee:addEmployee		
+	return {
+		addEmployee:addEmployee,
+		accessToken:accessToken,
+		loginProcess:loginProcess
 	}
 	
 })();
-
-
-
-
-
